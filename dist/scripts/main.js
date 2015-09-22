41ab708c1ea257b46c4f1159fe9e0195b3d48ba9
Noel Farris <noel.farris@gmail.com>
2015-09-22 14:52:42 -0500

    normal

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -16,3 +16,22 @@ var App = Backbone.Router.extend({
 
 var app = new App();
 Backbone.history.start();
+
+$('#name').val(user.get('name'))
+$('#inputEmail3').val(user.get('email'))
+$('#role').val(user.get('role'))
+
+$('form').submit(function(e) {
+	e.preventDefault();
+	user.set({
+		name: $('#name').val(),
+		email: $('#inputEmail3').val(),
+		role: $('#role').val()
+	})
+	console.log(user);
+})
+user.on('change', function() {
+	$('.profile-usertitle-job').html(user.get('role'));
+	$('.profile-usertitle-name').html(user.get('name'));
+	$('.dropdown-toggle').html(user.get('name'));
+})
